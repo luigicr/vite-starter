91b2243479cca21b2cec76586a3d41962d3084e8
Luis Monge <luigi17@gmail.com>
2024-05-02 21:27:06 -0600

    images day 2

--- src/banner.js
+++ src/banner.js
@@ -1,4 +1,7 @@
 import { banner } from './banner.module.css';
+import favicon from './favicon-16x16.png';
+
+console.log(favicon);
 
 /**
  * Adds a banner text to the element with id 'content'.
@@ -10,11 +13,20 @@ import { banner } from './banner.module.css';
  * addBanner('Welcome to our website!');
  */
 export const addBanner = (text) => {
-  const container = document.querySelector('#content');
-  container.classList.add(banner);
-  container.textContent = text;
-  
+  const content = document.querySelector('#content');
+  content.classList.add(banner);
+  content.textContent = text;
+
   // import('./banner.module.css').then(({ banner }) => {
   //     container.classList.add(banner);
   // });
+};
+
+import image from './steve-after-a-workshop.jpg?h=350';
+
+export const loadImage = () => {
+  const content = document.querySelector('#content');
+  const imageElement = document.createElement('img');
+  imageElement.src = image;
+  content.appendChild(imageElement);
 };
